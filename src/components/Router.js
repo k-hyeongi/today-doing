2093416cdd79d1aae745da6f113a6815b884c84d
k-hyeongi <21900215@handong.ac.kr>
2022-07-14 16:12:56 +0900

    0.1 Initialize Settings

---
+++ src/components/Router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+const AppRouter = (props) => {
+	return (
+		<Router>
+			<Routes>
+				<Route />
+			</Routes>
+		</Router>
+	);
+};
